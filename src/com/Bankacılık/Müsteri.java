0e658d871c38189a7d84f0b03f5cfcb6a8d2bdb7
bbece <bbeceok@hotmail.com>
2022-05-17 09:16:52 +0200

    Initial commit

---
+++ src/com/Bankacılık/Müsteri.java
@@ -0,0 +1,11 @@
+package com.Bankacılık;
+
+public class Müsteri {
+    private final String kullanici_adi;
+    private final int tc_No;
+
+    Müsteri(String kullanici_adi, int tc_no) {
+        this.kullanici_adi = kullanici_adi;
+        tc_No = tc_no;
+    }
+}
